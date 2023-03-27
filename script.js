6d4e573754b4a6fb40cded533b97a1019d07ac44
Prabhu <123736381+Prabhu67@users.noreply.github.com>
2023-03-27 10:18:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Countdown starts
+setTimeout(()=>{
+    document.getElementById('countdown').innerHTML = 10;
+    setTimeout(()=>{
+        document.getElementById('countdown').innerHTML = 9;
+        setTimeout(()=>{
+            document.getElementById('countdown').innerHTML = 8;
+            setTimeout(()=>{
+                document.getElementById('countdown').innerHTML = 7;
+                setTimeout(()=>{
+                    document.getElementById('countdown').innerHTML = 6;
+                    setTimeout(()=>{
+                        document.getElementById('countdown').innerHTML = 5;
+                        setTimeout(()=>{
+                            document.getElementById('countdown').innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById('countdown').innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById('countdown').innerHTML = 2;
+                                    setTimeout(()=>{
+                                        document.getElementById('countdown').innerHTML = 1;
+                                        setTimeout(()=>{
+                                            document.getElementById('countdown').innerHTML = '';
+                                            document.getElementById('img').src='indi.png'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},0)
